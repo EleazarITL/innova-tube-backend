a326f1c9752e733ab8d15bfbcf943fa56aa5cd8f
EleazarBiida <eleazar717@icloud.com>
2024-10-22 15:04:10 -0600

    Solución de SSL

--- src/database.ts
+++ src/database.ts
@@ -9,7 +9,10 @@ const pool = new Pool({
   host: process.env.PGHOST,
   database: process.env.PGDATABASE,
   password: process.env.PGPASSWORD,
-  port: Number(process.env.PGPORT)
+  port: Number(process.env.PGPORT),
+  ssl: {
+    rejectUnauthorized: false,
+  },
 });
 
 async function connectDB() {
